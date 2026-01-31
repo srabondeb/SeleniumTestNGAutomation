c893482ce2706174ace80f119c57c0c26281513f
srabondebnath <srabondebnath82@gmail.com>
2026-01-31 14:13:24 -0500

    Autocomplete form complete

--- src/test/java/AllTests/TestMethods.java
+++ src/test/java/AllTests/TestMethods.java
@@ -2,18 +2,30 @@ package AllTests;
 
 import TestSetBreak.BeforeAfterMethods;
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.Test;
 
 public class TestMethods extends BeforeAfterMethods {
     @Test(priority = 0)
-    public void testAutocomplete(){
+    public void testAutocomplete() throws InterruptedException {
         By Address = By.id("autocomplete");
-        By city = By.id("autocomplete");
-        By state = By.id("autocomplete");
-        By zip = By.id("autocomplete");
-        By country = By.id("autocomplete");
+        By city = By.id("locality");
+        By state = By.id("administrative_area_level_1");
+        By zip = By.id("postal_code");
+        By country = By.id("country");
 
         driver.get("https://formy-project.herokuapp.com/autocomplete");
         driver.findElement(Address).sendKeys("123 example Street");
+        driver.findElement(city).sendKeys("Manassas");
+        driver.findElement(state).sendKeys("Virginia");
+        driver.findElement(zip).sendKeys("20109");
+        driver.findElement(country).sendKeys("Prince William");
+
+        //wait 2 second to inspect all the boxes
+        Thread.sleep(2000);
     }
+
+
+
+
 }
--- src/test/java/TestSetBreak/BeforeAfterMethods.java
+++ src/test/java/TestSetBreak/BeforeAfterMethods.java
@@ -2,16 +2,23 @@ package TestSetBreak;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.time.Duration;
+
 public class BeforeAfterMethods {
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     @BeforeMethod
     public void setUp(){
         driver=new ChromeDriver();
+
         driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
     }
     @AfterMethod
     public void BreakDown(){
