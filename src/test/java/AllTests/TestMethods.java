12ecdbfe3c331e2d36f46363b538cef7803e04a7
srabondebnath <srabondebnath82@gmail.com>
2026-02-02 12:50:43 -0500

    completed ButtonTest and CheckBoxTest

--- src/test/java/AllTests/TestMethods.java
+++ src/test/java/AllTests/TestMethods.java
@@ -3,9 +3,11 @@ package AllTests;
 import TestSetBreak.BeforeAfterMethods;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestMethods extends BeforeAfterMethods {
+
     @Test(priority = 0)
     public void testAutocomplete() throws InterruptedException {
         By Address = By.id("autocomplete");
@@ -20,11 +22,42 @@ public class TestMethods extends BeforeAfterMethods {
         driver.findElement(state).sendKeys("Virginia");
         driver.findElement(zip).sendKeys("20109");
         driver.findElement(country).sendKeys("Prince William");
-        //wait 2 second to inspect all the boxes
         Thread.sleep(2000);
+
+        //used one example of assert statement to test the input value
+        Assert.assertEquals(
+                driver.findElement(Address).getAttribute("value"),"123 example Street"
+        );
+
     }
 
 
+    @Test(priority = 1)
+    public void ButtonTest() throws InterruptedException{
+        driver.get("https://formy-project.herokuapp.com/buttons");
+        By infoButton = By.xpath("/html/body/div/form/div[1]/div/div/button[3]");
+        driver.findElement(infoButton).click();
+        Thread.sleep(2000);
+
+        Assert.assertTrue(
+                driver.findElement(infoButton).isEnabled(),
+                "Info button was not activated after click"
+        );
+
+    }
+
+
+    @Test(priority = 2)
+    public void checkBoxTest() throws InterruptedException{
+        By box1 = By.id("checkbox-1");
+        driver.get("https://formy-project.herokuapp.com/checkbox");
+        driver.findElement(box1).click();
+        Thread.sleep(2000);
+
+        Assert.assertTrue(
+                driver.findElement(box1).isSelected(),"The box1 failed checked"
+        );
+    }
 
 
 }
